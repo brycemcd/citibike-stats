43b2c8785e83db11093824d7b0340d156f7385b3
Bryce McDonnell <bryce@bridgetownint.com>
2014-05-11 11:37:23 -0400

    adds trip heatmap

---
+++ time_of_day_usage.R
@@ -0,0 +1,15 @@
+# Density of days and hours
+library(reshape2)
+
+citi$hour_trip_started <- strftime(citi$starttime, format= "%H")
+citi$dow_trip_started  <- strftime(citi$starttime, format= "%a")
+
+den <- melt(table(citi$hour_trip_started, citi$dow_trip_started))
+names(den) <- c("hour", "day", "c")
+#den$day <- factor(den$day, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
+
+p <- ggplot(data=den, aes(x=factor(day, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")), y=hour)) +
+  geom_tile(aes(fill=c)) +
+  scale_fill_gradient(low="blue", high="red2") +
+  labs(x="Day of Week", title="Density of trips started", y="Hour of Day")
+p
